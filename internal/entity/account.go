dc1c7b762b982ce813adef75d6f3af6665f80650
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-23 21:47:07 -0300

    feat: credit and debit for account

--- internal/entity/account.go
+++ internal/entity/account.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,12 +15,35 @@ type Account struct {
 	UpdatedAt time.Time
 }
 
-func NewAccount(client *Client, balance float64) (*Account, error) {
+func NewAccount(client *Client) (*Account, error) {
+	if client == nil {
+		return nil, errors.New("invalid client")
+	}
+
 	return &Account{
 		ID:        uuid.New().String(),
 		Client:    client,
-		Balance:   balance,
+		Balance:   0,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}, nil
+}
+
+func (a *Account) Credit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("invalid amount to credit")
+	}
+
+	a.Balance += amount
+	a.UpdatedAt = time.Now()
+	return nil
+}
+
+func (a *Account) Debit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("invalid amount to credit")
+	}
+	a.Balance -= amount
+	a.UpdatedAt = time.Now()
+	return nil
 }
