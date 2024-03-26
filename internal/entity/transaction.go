b99ad60925d61fcd1724a2357ce55403f628995a
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-25 22:48:01 -0300

    refactor: vaidate transaction

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -17,22 +17,36 @@ type Transaction struct {
 
 func NewTransaction(accountFrom, accountTo *Account, amount float64) (*Transaction, error) {
 
-	if accountFrom == nil {
-		return nil, errors.New("Account from cannot be empty")
-	}
-
-	if accountTo == nil {
-		return nil, errors.New("Account to cannot be empty")
-	}
-
-	if amount <= 0 {
-		return nil, errors.New("Amount cannot be less than or equal to 0")
-	}
-
-	return &Transaction{
+	transaction := &Transaction{
 		ID:          uuid.New().String(),
 		AccountFrom: accountFrom,
 		AccountTo:   accountTo,
 		Amount:      amount,
-	}, nil
+	}
+
+	if err := transaction.Validate(); err != nil {
+		return nil, err
+	}
+
+	return transaction, nil
+}
+
+func (t *Transaction) Validate() error {
+	if t.AccountFrom == nil {
+		return errors.New("account from cannot be empty")
+	}
+
+	if t.AccountTo == nil {
+		return errors.New("account to cannot be empty")
+	}
+
+	if t.Amount <= 0 {
+		return errors.New("amount cannot be less than or equal to 0")
+	}
+
+	if t.AccountFrom.Balance < t.Amount {
+		return errors.New("insuficient funds")
+	}
+
+	return nil
 }
