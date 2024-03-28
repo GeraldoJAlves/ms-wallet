82c2a87c0f051c388caa4132fb6c28410c35e7ca
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-28 08:32:58 -0300

    feat: create account use case

--- internal/gateway/account.go
+++ internal/gateway/account.go
@@ -3,6 +3,6 @@ package gateway
 import "github.com/geraldojalves/ms-wallet/internal/entity"
 
 type AccountGateway interface {
-	Save(account entity.Account) error
+	Save(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
 }
---
+++ internal/usecase/create_account.go
@@ -0,0 +1,47 @@
+package usecase
+
+import (
+	"github.com/geraldojalves/ms-wallet/internal/entity"
+	"github.com/geraldojalves/ms-wallet/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (c *CreateAccountUseCase) Execute(input *CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := c.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := entity.NewAccount(client)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
